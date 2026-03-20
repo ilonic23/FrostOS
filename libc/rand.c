5a08cf6096a00594a2156a5e459d998af18d7aa3
ilonic23 <ilia.pwl@gmail.com>
2026-03-20 18:55:50 +0000

    Rewrite rand.h/rand.c

---
+++ libc/rand.c
@@ -0,0 +1,31 @@
+#include "rand.h"
+#include <stdint.h>
+
+static uint64_t state = 0x4d595df4d0f33173;
+static uint64_t const multiplier = 6364136223846793005u;
+static uint64_t const increment = 1442695040888963407u;
+
+static uint32_t rotr32(uint32_t x, unsigned r) {
+    return x >> r | x << (-r & 31); 
+}
+
+uint32_t pcg32(void) { 
+    uint64_t x = state;
+    unsigned count = (unsigned)(x >> 59); // 59 = 64 - 5 
+    state = x * multiplier + increment;
+    x ^= x >> 18; // 18 = (64 - 27)/2 
+    return rotr32((uint32_t)(x >> 27), count); // 27 = 32 - 5 
+}
+
+void pcg32_init(uint64_t seed) {
+    state = seed + increment; 
+    (void)pcg32(); 
+}
+
+uint32_t pcg32_lim(uint32_t low, uint32_t high) {
+    uint32_t boundary = high - low + 1;
+    uint32_t threshold = (UINT32_MAX - boundary + 1) % boundary;
+    uint32_t r;
+    while ((r = pcg32()) < threshold);
+    return low + (r % boundary);
+}
