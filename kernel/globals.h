3987bf4f25afd16117da60fdb1df4e1cb1eb375d
Evelyn Snow <snow.snow3@proton.me>
2026-04-13 05:06:40 +0000

    Add fields to kernel globals

--- kernel/globals.h
+++ kernel/globals.h
@@ -1,14 +1,19 @@
-#include <stdint.h>
-#include "../multiboot/multiboot.h"
 #include "../drivers/clock.h"
+#include "../drivers/storage/drive.h"
+#include "../multiboot/multiboot.h"
+#include <stdint.h>
 
 typedef struct {
     multiboot_info_t *multiboot_addr;
-    datetime_t        datetime;
-    uint8_t           cpuid_supported;
-    char              cpu_manufacturer[13];
-    char              cpu_full_name[49];
-    char             *kernel_name;
-    char             *kernel_version;
-    char             *kernel_codename;
+    datetime_t datetime;
+    uint8_t cpuid_supported;
+    char cpu_manufacturer[13];
+    char cpu_full_name[49];
+    char *kernel_name;
+    char *kernel_version;
+    char *kernel_codename;
+    drive_entry_t *drives;
+    uint16_t drives_count;
+    uint16_t *pci_devs;
+    uint16_t pci_dev_count;
 } kernel_globals;
