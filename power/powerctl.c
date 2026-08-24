310c43806d2507e60480cadb42bf145df2cbcd23
Evelyn Snow <snow.snow3@proton.me>
2026-08-24 11:10:07 +0300

    Shutting down is now halting

--- power/powerctl.c
+++ power/powerctl.c
@@ -1,7 +1,7 @@
 #include "powerctl.h"
-#include "acpi.h"
 #include "../cpu/ports.h"
 #include "../libc/stdio.h"
+#include "acpi.h"
 
 void halt() {
 loop:
@@ -9,7 +9,8 @@ loop:
     goto loop;
 }
 
-// THE CODE TOOK FROM OSDEV WIKI: https://wiki.osdev.org/Reboot#Annotated_code_for_reboot
+// THE CODE TOOK FROM OSDEV WIKI:
+// https://wiki.osdev.org/Reboot#Annotated_code_for_reboot
 
 /* keyboard interface IO port: data and control
    READ:   status port
@@ -17,26 +18,26 @@ loop:
 #define KBRD_INTRFC 0x64
 
 /* keyboard interface bits */
-#define KBRD_BIT_KDATA 0 /* keyboard data is in buffer (output buffer is empty) (bit 0) */
-#define KBRD_BIT_UDATA 1 /* user data is in buffer (command buffer is empty) (bit 1) */
+#define KBRD_BIT_KDATA                                                         \
+    0 /* keyboard data is in buffer (output buffer is empty) (bit 0) */
+#define KBRD_BIT_UDATA                                                         \
+    1 /* user data is in buffer (command buffer is empty) (bit 1) */
 
-#define KBRD_IO 0x60 /* keyboard IO port */
+#define KBRD_IO 0x60    /* keyboard IO port */
 #define KBRD_RESET 0xFE /* reset CPU command */
 
-#define bit(n) (1<<(n)) /* Set bit n to 1 */
+#define bit(n) (1 << (n)) /* Set bit n to 1 */
 
 /* Check if bit n in flags is set */
 #define check_flag(flags, n) ((flags) & bit(n))
 
-void reboot()
-{
+void reboot() {
     uint8_t temp;
 
-    asm volatile ("cli"); /* disable all interrupts */
+    asm volatile("cli"); /* disable all interrupts */
 
     /* Clear all keyboard buffers (output and command buffers) */
-    do
-    {
+    do {
         temp = port_byte_in(KBRD_INTRFC); /* empty user data */
         if (check_flag(temp, KBRD_BIT_KDATA) != 0)
             port_byte_in(KBRD_IO); /* empty keyboard data */
@@ -44,26 +45,25 @@ void reboot()
 
     port_byte_out(KBRD_INTRFC, KBRD_RESET); /* pulse CPU reset line */
 loop:
-    asm volatile ("hlt"); /* if that didn't work, halt the CPU */
-    goto loop; /* if a NMI is received, halt again */
+    asm volatile("hlt"); /* if that didn't work, halt the CPU */
+    goto loop;           /* if a NMI is received, halt again */
 }
 
 // -----------------------------------------------------------------------------
 
 void poweroff() {
-    rsdp_t *rsdp = find_rsdp();
-    if (!rsdp) halt();
-
-    fadt_t *fadt = find_fadt(rsdp);
-    if (!fadt) halt();
-
-    acpi_enable(fadt);
-
-    if (!parse_slp_from_dsdt(fadt)) halt();
-
-    acpi_shutdown(fadt);
-
-    // We shall not be here
-    printf("[[Hyperlink Blocked]]\n");
+    // rsdp_t *rsdp = find_rsdp();
+    // if (!rsdp) halt();
+    //
+    // fadt_t *fadt = find_fadt(rsdp);
+    // if (!fadt) halt();
+    //
+    // acpi_enable(fadt);
+    //
+    // if (!parse_slp_from_dsdt(fadt)) halt();
+    //
+    // acpi_shutdown(fadt);
+    //
+    // // We shall not be here
     halt();
 }
