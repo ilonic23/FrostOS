4cc3477570fa500d81564584bccd10be60efa6b9
Evelyn Snow <snow.snow3@proton.me>
2026-09-12 09:58:31 +0000

    Add trace_empty

--- kernel/debug.h
+++ kernel/debug.h
@@ -5,8 +5,10 @@
 void kernel_trace(char *format, ...);
 #define trace(fmt, ...)                                                        \
     kernel_trace("[TRACE] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define trace_empty(fmt, ...) kernel_trace(fmt, ##__VA_ARGS__)
 #else
 #define trace(fmt, ...) ((void)0)
+#define trace_empty(fmt, ...) ((void)0)
 #endif
 
 #endif
