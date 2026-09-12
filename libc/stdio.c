0cbb34e46d096e6d9230ae6beb242fc5263218a3
ilonic23 <ilia.pwl@gmail.com>
2026-09-12 14:23:20 +0300

    Reformat stdio.c

--- libc/stdio.c
+++ libc/stdio.c
@@ -1,9 +1,9 @@
 #include "./stdio.h"
-#include "./ctype.h"
 #include "../drivers/display/display.h"
-#include <stdint.h>
-#include <stdarg.h>
+#include "./ctype.h"
 #include "./stdlib.h"
+#include <stdarg.h>
+#include <stdint.h>
 
 void printf(const char *format, ...) {
     va_list args;
@@ -21,25 +21,26 @@ void printf(const char *format, ...) {
                     int64_t v = va_arg(args, int64_t);
                     char buf[32];
                     itoa(v, buf, 10);
-                    for (char *p = buf; *p; p++) display_print_char_ez(*p);
-                }
-                else if (*format == 'u') {
+                    for (char *p = buf; *p; p++)
+                        display_print_char_ez(*p);
+                } else if (*format == 'u') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 10);
-                    for (char *p = buf; *p; p++) display_print_char_ez(*p);
-                }
-                else if (*format == 'x') {
+                    for (char *p = buf; *p; p++)
+                        display_print_char_ez(*p);
+                } else if (*format == 'x') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 16);
-                    for (char *p = buf; *p; p++) display_print_char_ez(*p);
-                }
-                else if (*format == 'o') {
+                    for (char *p = buf; *p; p++)
+                        display_print_char_ez(*p);
+                } else if (*format == 'o') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 8);
-                    for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                    for (char *p = buf; *p; p++)
+                        display_print_char_ez(*p);
                 }
 
                 format++;
@@ -51,49 +52,56 @@ void printf(const char *format, ...) {
                 int32_t v = va_arg(args, int32_t);
                 char buf[32];
                 itoa(v, buf, 10);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             }
             // ---- %u ----
             else if (*format == 'u') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 10);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             }
             // ---- %x ----
             else if (*format == 'x') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 16);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             }
             // ---- %X ----
             else if (*format == 'X') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 16);
-                for (char *p = buf; *p; p++) display_print_char_ez(toupper(*p));
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(toupper(*p));
             }
             // ---- %o ----
             else if (*format == 'o') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 8);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             }
             // ---- %f ----
             else if (*format == 'f') {
                 double v = va_arg(args, double);
                 char buf[64];
                 ftoa(v, buf, 10, 6);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             } else if (*format == 'p') {
                 void *v = va_arg(args, void *);
                 char buf[32];
                 display_print_char_ez('0');
                 display_print_char_ez('x');
                 itoa((uintptr_t)v, buf, 16);
-                for (char *p = buf; *p; p++) display_print_char_ez(*p);
+                for (char *p = buf; *p; p++)
+                    display_print_char_ez(*p);
             }
             // ---- %c ----
             else if (*format == 'c') {
@@ -103,7 +111,8 @@ void printf(const char *format, ...) {
             // ---- %s ----
             else if (*format == 's') {
                 char *v = va_arg(args, char *);
-                for (char *p = v; *p; p++) display_print_char_ez(*p);
+                for (char *p = v; *p; p++)
+                    display_print_char_ez(*p);
             }
             // --- %% ---
             else if (*format == '%') {
@@ -111,8 +120,7 @@ void printf(const char *format, ...) {
             }
 
             format++;
-        }
-        else {
+        } else {
             display_print_char_ez(*format++);
         }
     }
@@ -138,25 +146,26 @@ int sprintf(char *s, const char *format, ...) {
                     int64_t v = va_arg(args, int64_t);
                     char buf[32];
                     itoa(v, buf, 10);
-                    for (char *p = buf; *p; p++) *s++ = *p;
-                }
-                else if (*format == 'u') {
+                    for (char *p = buf; *p; p++)
+                        *s++ = *p;
+                } else if (*format == 'u') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 10);
-                    for (char *p = buf; *p; p++) *s++ = *p;
-                }
-                else if (*format == 'x') {
+                    for (char *p = buf; *p; p++)
+                        *s++ = *p;
+                } else if (*format == 'x') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 16);
-                    for (char *p = buf; *p; p++) *s++ = *p;
-                }
-                else if (*format == 'o') {
+                    for (char *p = buf; *p; p++)
+                        *s++ = *p;
+                } else if (*format == 'o') {
                     uint64_t v = va_arg(args, uint64_t);
                     char buf[32];
                     itoa(v, buf, 8);
-                    for (char *p = buf; *p; p++) *s++ = *p;
+                    for (char *p = buf; *p; p++)
+                        *s++ = *p;
                 }
 
                 format++;
@@ -168,49 +177,56 @@ int sprintf(char *s, const char *format, ...) {
                 int32_t v = va_arg(args, int32_t);
                 char buf[32];
                 itoa(v, buf, 10);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             }
             // ---- %u ----
             else if (*format == 'u') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 10);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             }
             // ---- %x ----
             else if (*format == 'x') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 16);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             }
             // ---- %X ----
             else if (*format == 'X') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 16);
-                for (char *p = buf; *p; p++) *s++ = toupper(*p);
+                for (char *p = buf; *p; p++)
+                    *s++ = toupper(*p);
             }
             // ---- %o ----
             else if (*format == 'o') {
                 uint32_t v = va_arg(args, uint32_t);
                 char buf[32];
                 itoa(v, buf, 8);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             }
             // ---- %f ----
             else if (*format == 'f') {
                 double v = va_arg(args, double);
                 char buf[64];
                 ftoa(v, buf, 10, 6);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             } else if (*format == 'p') {
                 void *v = va_arg(args, void *);
                 char buf[32];
                 *s++ = '0';
                 *s++ = 'x';
                 itoa((uintptr_t)v, buf, 16);
-                for (char *p = buf; *p; p++) *s++ = *p;
+                for (char *p = buf; *p; p++)
+                    *s++ = *p;
             }
             // ---- %c ----
             else if (*format == 'c') {
@@ -220,7 +236,8 @@ int sprintf(char *s, const char *format, ...) {
             // ---- %s ----
             else if (*format == 's') {
                 char *v = va_arg(args, char *);
-                for (char *p = v; *p; p++) *s++ = *p;
+                for (char *p = v; *p; p++)
+                    *s++ = *p;
             }
             // --- %% ---
             else if (*format == '%') {
@@ -228,8 +245,7 @@ int sprintf(char *s, const char *format, ...) {
             }
 
             format++;
-        }
-        else {
+        } else {
             *s++ = *format++;
         }
     }
