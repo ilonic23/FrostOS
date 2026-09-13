b248e28626ea8f6f786e17d5022e2f7284bbbae2
ilonic23 <ilia.pwl@gmail.com>
2026-09-13 16:25:18 +0300

    Add debug.h to include/kor/

---
+++ include/kor/debug.h
@@ -0,0 +1,14 @@
+#ifndef KOR_DEBUG_H
+#define KOR_DEBUG_H
+
+#ifdef KERNEL_DEBUG
+void kernel_trace(char *format, ...);
+#define trace(fmt, ...)                                                        \
+    kernel_trace("[TRACE] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define trace_empty(fmt, ...) kernel_trace(fmt, ##__VA_ARGS__)
+#else
+#define trace(fmt, ...) ((void)0)
+#define trace_empty(fmt, ...) ((void)0)
+#endif
+
+#endif // KOR_DEBUG_H
