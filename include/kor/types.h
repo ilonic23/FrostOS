5c1ee74125dc362d4ba56a7ab9bf5e95b165fbfa
ilonic23 <ilia.pwl@gmail.com>
2026-09-13 12:08:07 +0300

    Add include directory, types.h

---
+++ include/kor/types.h
@@ -0,0 +1,21 @@
+#ifndef KOR_TYPES_H
+#define KOR_TYPES_H
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned u32;
+typedef unsigned long long u64;
+
+typedef float f32;
+typedef double f64;
+typedef long double f96;
+
+typedef int size;
+typedef unsigned int usize;
+
+#endif // KOR_TYPES_H
