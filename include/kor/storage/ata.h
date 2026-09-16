eab96c7dfdf83f1bee6521140d83389dc11e7d99
ilonic23 <ilia.pwl@gmail.com>
2026-09-16 11:22:12 +0300

    Rename ata_stro_c to ata_str_to_c

--- include/kor/storage/ata.h
+++ include/kor/storage/ata.h
@@ -11,7 +11,7 @@ typedef struct {
 } ata_drive_t;
 
 int ata_identify(u8 secondary, u8 slave, ata_drive_t *drive);
-void ata_stro_c(char *dest, const u16 *src, int words);
+void ata_str_to_c(char *dest, const u16 *src, int words);
 u32 ata_get_lba28_sects(ata_drive_t *drive);
 u64 ata_get_lba48_sects(ata_drive_t *drive);
 int ata_lba28_read(ata_drive_t *drive, u32 lba, u8 count, u16 *buffer);
